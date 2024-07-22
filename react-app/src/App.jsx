b395a9f6e69735ebe28efbc63e68c5adfe4f5503
sai <ojiip95@gmail.com>
2024-07-23 00:02:57 +0200

    build: react app with vite, did minor setup for eslint and prettier

---
+++ react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Login from "./components/pages/home";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/pages/home.jsx
@@ -0,0 +1,11 @@
+import Button from "../button";
+function Login() {
+  return (
+    <>
+      <h1>login from here</h1>
+      <Button />
+    </>
+  );
+}
+
+export default Login;
