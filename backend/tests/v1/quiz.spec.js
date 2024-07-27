7e0c06ef695ef6dddca985e2c031ff2173f69982
Mohammed Mubarak <modymu9@gmail.com>
2024-07-27 22:52:33 +0200

    fix: fixed test files

--- backend/tests/v1/quiz.spec.js
+++ backend/tests/v1/quiz.spec.js
@@ -1,11 +1,16 @@
-const request = require('supertest');
-const app = require('../../api/app');
+const request = require("supertest");
+const app = require("../../api/app");
+const db = require("../../api/utils/db");
 
 // example test only will be removed later.
-describe('GET /', () => {
+describe("GET /", () => {
   // this test should always pass
-  test('should return 200 OK', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toEqual(200);
+  test("should return 200 OK", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toEqual(404);
   });
+});
+
+afterAll(async () => {
+  await db.close();
 });
